af5f350d6884ea687f39e5cb28274738de118bcb
bakushiin <braderdely@gmail.com>
2025-09-26 13:14:01 -0600

    forgot to save the fix

--- app/src/main/java/com/example/breads/Shape.java
+++ app/src/main/java/com/example/breads/Shape.java
@@ -3,10 +3,8 @@ package com.example.breads;
 public abstract class Shape {
     protected int x;
     protected int y;
-<<<<<<< HEAD
-    String color = "orange";
-=======
+
     String color = "Pink";
->>>>>>> 289d18e790c4856525588c43839d3ce94df9eced
+
 }
 
